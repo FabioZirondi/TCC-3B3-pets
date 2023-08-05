7383e7bb2d8ac3dc329b63e5f9101711bccbca74
FabioZirondi <fabio96zirondi@gmail.com>
2023-08-05 14:47:34 -0300

    começei php

---
+++ php/conexao.php
@@ -0,0 +1,13 @@
+<?php
+
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$bd = "bdanimais";
+
+$conn = mysqli_connect($servidor, $usuario, $senha, $bd);
+
+if(!$conn) {
+    die("Falha de conexão: " . mysqli_connect_error());
+}
+?>

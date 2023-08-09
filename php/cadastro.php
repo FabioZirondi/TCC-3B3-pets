8a2d6d4171c55fe33a89923d5960a92588be8613
FabioZirondi <fabio96zirondi@gmail.com>
2023-08-09 16:05:56 -0300

    0908 - cadastro funcionando 100%

--- php/cadastro.php
+++ php/cadastro.php
@@ -1,34 +1,37 @@
 <?php
-
+// importa as variáveis do html
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $usersenha = $_POST['senha'];
 
+// faz conexão com banco de dados
 include_once("../php/conexao.php");
 
 $nome = mysqli_real_escape_string($conn, $nome);
 $email = mysqli_real_escape_string($conn, $email);
 $senha = mysqli_real_escape_string($conn, $senha);
 
-
-
-if (!empty($nome) && !empty($email) && !empty($usersenha)) {
-
-    $sql = $pdo->prepare("SELECT * FROM usuario WHERE email = := email");
-    $sql->bindValue(":email", $email);
-    $sql->execute();
-
-    if ($sql->rowCount() === 0) {
-        $senha_hash = password_hash($usersenha, PASSWORD_DEFAULT);
-        $stmt = "INSERT INTO usuario (nome, email, senha) VALUES ('$nome', '$email', '$senha_hash');";
-    }else{
-        header("Location: ../php/cadastrohtml.php");
-    }
-
-} else {
-    echo "Erro: A senha não pode estar vazia.";
+// Verifica se algum campo está vazio
+if (empty($nome) || empty($email) || empty($usersenha)) {
+    echo "Erro: Todos os campos devem estar preenchidos.";
     exit;
 }
+
+// Verifica se o e-mail já está em uso
+$email_query = "SELECT * FROM usuario WHERE email = '$email'";
+$email_result = mysqli_query($conn, $email_query);
+
+if (mysqli_num_rows($email_result) > 0) {
+    echo "Erro: Este e-mail já está cadastrado.";
+    exit;
+}
+
+// Criação do hash da senha
+$senha_hash = password_hash($usersenha, PASSWORD_DEFAULT);
+
+// Inserção do usuário no banco de dados
+$stmt = "INSERT INTO usuario (nome, email, senha) VALUES ('$nome', '$email', '$senha_hash')";
+
 
 if (mysqli_query($conn, $stmt)) {
     header('Location: ../php/catalogo.php');
@@ -38,4 +41,5 @@ if (mysqli_query($conn, $stmt)) {
 }
 
 mysqli_close($conn);
+
 ?>

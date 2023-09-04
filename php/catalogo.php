4c7bab46e36056ac2bddba19e1dca2bfd156e07e
FabioZirondi <fabio96zirondi@gmail.com>
2023-09-04 12:06:20 -0300

    camilly arrumou o footer

--- php/catalogo.php
+++ php/catalogo.php
@@ -6,56 +6,56 @@ include_once("../php/verificaSession.php");
 
 <!DOCTYPE html>
 <html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/HomePage.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Dosis&family=Montserrat:wght@100;200&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../css/catalogo.css">
-    <title>Página Inicial</title>
-</head>
-
-<body>
-    <header>
-        <div class="topnav" id="myTopnav">
-            <a href="../php/HomePage.php" class="active">I-Pet</a>
-            <a href="../php/logout.php"> <button class="button" type="button">Sair</button></a>
-            <a href="#sobre">Sobre</a>
-            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-                <i class="fa fa-bars"></i>
-            </a>
-        </div>
-    </header>
-    <main>
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/HomePage.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Dosis&family=Montserrat:wght@100;200&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="../css/catalogo.css">
+        <title>Página Inicial</title>
+    </head>
+    
+    <body>
+        <header>
+            <div class="topnav" id="myTopnav">
+                <a href="../php/HomePage.php" class="active">I-Pet</a>
+                <a href="../php/logout.php"> <button class="button" type="button">Sair</button></a>
+                <a href="#sobre">Sobre</a>
+                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                    <i class="fa fa-bars"></i>
+                </a>
+            </div>
+        </header>
+        <main>
+            </br>
         </br>
-        </br>
-        </br>
-        <div class="card">
-            <img src="../img/servicobanho.jpg" alt="Denim Jeans" style="width:100%">
-            <h1>Tailored Jeans</h1>
-            <p class="price">$19.99</p>
-            <p>Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem jeansum.</p>
-            <p><button>Add to Cart</button></p>
-        </div>
-
-    </main>
-    <footer>
-        <h1>I-Pet</h1>
-        <p>I-pet@gmail.com</p>
-        <div class="social-icons">
-            <a href="https://pt-br.facebook.com/"><img src="../img/icon-facebook.png" alt="Facebook"></a>
-            <a href="https://twitter.com/"><img src="../img/icon-twitter.png" alt="Twitter"></a>
-            <a href="https://br.linkedin.com/"><img src="../img/icon-linkedin.png" alt="LinkedIn"></a>
-            <a href="https://web.telegram.org/a/"><img src="../img/icon-telegram.png" alt="Telegram"></a>
-        </div>
-        <p>&copy; 2023. Todos os direitos reservados.</p>
-    </footer>
-    <script src="../js/HomePage.js"></script>
+    </br>
+    <div class="card">
+        <img src="../img/servicobanho.jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Tailored Jeans</h1>
+        <p>Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem jeansum.</p>
+        <p class="price">R$19.99</p>
+        <p><button>Add to Cart</button></p>
+    </div>
+    
+</main>
+<footer>
+    <h1>I-Pet</h1>
+    <p>I-pet@gmail.com</p>
+    <div class="social-icons">
+        <a href="https://pt-br.facebook.com/"><img src="../img/icon-facebook.png" alt="Facebook"></a>
+        <a href="https://twitter.com/"><img src="../img/icon-twitter.png" alt="Twitter"></a>
+        <a href="https://br.linkedin.com/"><img src="../img/icon-linkedin.png" alt="LinkedIn"></a>
+        <a href="https://web.telegram.org/a/"><img src="../img/icon-telegram.png" alt="Telegram"></a>
+    </div>
+    <p>&copy; 2023. Todos os direitos reservados.</p>
+</footer>
+<script src="../js/HomePage.js"></script>
 </body>
 
 </html>
